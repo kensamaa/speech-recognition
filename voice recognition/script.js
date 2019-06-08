fff5bcc8f777188e6fcc258006f4e63fd55e9444
amine abouothmane <amineab203@gmail.com>
2019-06-08 00:20:48 +0000

    Add files via upload

---
+++ voice recognition/script.js
@@ -0,0 +1,61 @@
+const btn = document.querySelector('.bt');
+const user=document.querySelector('.user');
+const him= document.querySelector('.content');
+
+const speechrecognition= window.speechrecognition || window.webkitSpeechRecognition;
+const recognition=new speechrecognition();
+
+const greetings=['i m good thanks','i m doing good','not bad'];
+const answerhaj='she is very pretty and sexy with a big pussy but her head is like a rock and she gets angry so fast';
+const fu=['dont be sad you are strong','no its ok just ignore the world','i m here for you','no stop be happy'];
+recognition.onstart=function(){
+    console.log("voice activated you can talk to microphone");
+    //readLoud('hello amine    start speaking');
+};
+
+//when u stop talking
+recognition.onresult=function(event){
+    const current=event.resultIndex;
+    const transcript=event.results[current][0].transcript;
+    user.textContent=transcript;
+    
+    //readLoud("u said ");
+    readLoud(transcript);
+};
+
+//add listener to the button
+btn.addEventListener('click',()=>{
+    recognition.start();
+});
+
+function readLoud(message){
+    const speech=new SpeechSynthesisUtterance();
+    speech.text='i dont know what you said';
+    if(message.includes('how are you')){
+        const textt=greetings[Math.floor(Math.random()*greetings.length)];
+        him.textContent=textt;
+        speech.text=textt;
+    }
+    else if(message.includes('what do you think')){
+        speech.text=answerhaj;
+        him.textContent=answerhaj;
+
+    }
+    else if(message.includes('sad')){
+        const textt=fu[Math.floor(Math.random()*greetings.length)];
+        him.textContent=textt;
+        speech.text=textt;
+    }
+    else if(message.includes('say')){
+        textt='thank you and i wish you very good luck in your study and your life '
+        him.textContent=textt;
+        speech.text=textt
+    }
+    speech.volume=1;
+    speech.rate=1;
+    speech.pitch=1;
+    speech.lang = 'en-US';
+    
+    
+    window.speechSynthesis.speak(speech);
+};
